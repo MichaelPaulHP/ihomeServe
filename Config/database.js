d9eb6b51455a12e85ba0ede2b1cb35f723db468b
MichaelPaulHP <michael_h_p_@hotmail.com>
2019-06-20 00:43:22 -0500

    add db

--- Config/database.js
+++ Config/database.js
@@ -3,10 +3,9 @@ const PASS="michael123";
 
 //mongodb://localhost/ihome
 //mongodb://<dbuser>:<dbpassword>@ds239967.mlab.com:39967/ihome
-var url="mongodb://"+USER+":"+PASS+"@s239967.mlab.com:39967/ihome";
+var url="mongodb://"+USER+":"+PASS+"@ds239967.mlab.com:39967/ihome";
 module.exports = {
     'user':USER,
     'pass':PASS,
     'url' : url 
-
 };
